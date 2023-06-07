bb7df79926af665306b14c25383702513dd303cb
vishalok <vrsafetech@gmail.com>
2023-06-07 19:26:36 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+app.use(cors());
 const Book = require('./models/books.models');
 const { PORT } = require('./configs/server.config');
 const { DB_URL, DB_PROD_URL } = require('./configs/db.config');
